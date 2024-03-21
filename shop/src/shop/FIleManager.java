77a047961dbb07b95213e8a46dbd6e1c3009e9ca
mingikim-giv <mingikim.giv@gmail.com>
2024-03-21 15:50:45 +0900

    Initial Commit

---
+++ shop/src/shop/FIleManager.java
@@ -0,0 +1,10 @@
+package shop;
+
+public class FIleManager {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ shop/src/shop/Main.java
@@ -0,0 +1,26 @@
+package shop;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Shop shop = new Shop("Mega");
+		shop.run();
+		
+		// Shop Project
+		// class : Item, ItemManager, User, UserManager, Cart, Shop, FileManager
+		
+		// method :
+		
+		// 유저 -
+		// ㄴ 회원가입/탈퇴
+		// ㄴ 로그인/로그아웃
+		// ㄴ 쇼핑하기
+		// ㄴ 마이페이지(내 장바구니, 항목 삭제, 수령 수정, 결제)
+		// ㄴ 자동 저장/ 자동 로드
+		
+		// 관리자 -
+		// ㄴ 아이템 등록/삭제/수정
+		// ㄴ 조회(총 매출)
+	}
+
+}
