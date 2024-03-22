d728f41baa335cbff28663ad3f91f5c718529b64
mingikim-giv <mingikim.giv@gmail.com>
2024-03-22 11:55:53 +0900

    Modify: ItemManager Class

--- shop/src/shop/ItemManager.java
+++ shop/src/shop/ItemManager.java
@@ -1,10 +1,47 @@
 package shop;
 
+import java.util.ArrayList;
+
 public class ItemManager {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private static ArrayList<Item> list;
+	private static ItemManager instance = new ItemManager();
+	
+	public static ItemManager getInstance() {
+		return instance;
 	}
-
+	
+	// searchItem
+	public boolean searchItem(String name) {
+		for(int i = 0; i < list.size(); i ++) {
+			Item item = list.get(i);
+			if(item.getName().equals(name)) {
+				return true;
+			}
+		}
+		return false;
+	}
+	
+	// C.
+	public void addItem(String name, int price) {
+		if(searchItem(name)) {
+			System.err.println("중복된 아이템 이름입니다.");
+			return;
+		}
+		Item item = new Item(name, price);
+		list.add(item);
+	}
+	// R.
+	public Item getItem(int idx) {
+		Item item = list.get(idx);
+		return item.clone();
+	}
+	// D.
+	public void removeItem(int idx) {
+		if(idx < 0 || idx >= list.size()) {
+			System.err.println("유효하지 않은 범위입니다.");
+			return;
+		}
+		list.remove(idx);
+	}
+	
 }
