7a46878cc4058a185348a68d34db1db332919236
mingikim-giv <mingikim.giv@gmail.com>
2024-03-22 14:14:58 +0900

    Modify: Cart Class

--- shop/src/shop/Cart.java
+++ shop/src/shop/Cart.java
@@ -1,10 +1,25 @@
 package shop;
 
+import java.util.ArrayList;
+
 public class Cart {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private ArrayList<Item> list;
+	private String id;
+	
+	public Cart(String id) {
+		this.id = id;
+		list = new ArrayList<>();
 	}
-
+	
+	public String getId() {
+		return this.id;
+	}
+	
+	public ArrayList<Item> getList() {
+		return this.list;
+	}
+	
+	public void addItem(Item item) {
+		this.list.add(item);
+	}
 }
