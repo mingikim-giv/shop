2832108160b5085c0fc46a92a4e3b7157a358427
mingikim-giv <mingikim.giv@gmail.com>
2024-03-22 11:11:12 +0900

    Modify: Item Class

--- shop/src/shop/Item.java
+++ shop/src/shop/Item.java
@@ -1,10 +1,29 @@
 package shop;
 
 public class Item {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	private String name;
+	private int price;
+	
+	public Item(String name, int price) {
+		this.name = name;
+		this.price = price;
 	}
-
+	
+	public String getName() {
+		return this.name;
+	}
+	
+	public int getPrice() {
+		return this.price;
+	}
+	
+	public Item clone() {
+		Item item = new Item(this.name, this.price);
+		return item;
+	}
+	
+	@Override
+	public String toString() {
+		return String.format("%s: %d원", this.name, this.price);
+	}
 }
